ff64057d3138a6ed3cb8a3b93f4a843345c31418
bluerune <b.sofia.sosa@gmail.com>
2024-08-14 21:10:25 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,40 +5,66 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
-    let controlador
+
     let password1 = document.getElementById("password1");
     let password2 = document.getElementById("password2");
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let email = document.getElementById("email");
+    let terminos = document.getElementById("terminos");
 // Funcion que llama a todas las validaciones
 function validaciones(){
-    let inputs = document.getElementsByTagName("input");
-    let terminosAceptados = document.getElementById('terminos').checked;
-
-    
-    for (let i = 0; i < inputs.length; i++) {
-        validarVacio(i);
-    }
-
-    if (controlador=0 && validarContrasenias && terminosAceptados && cantidadDecaracteres){
+    console.log("Validaciones")
+    if (validarVacio() && validarContrasenias() && terminosChecked() && cantidadDecaracteres()){
+        console.log("success");
         showAlertSuccess();
     } else {
+        console.log("error");
         showAlertError();
     }
 }
 
 //Checar que no hayan campos vacios
-function validarVacio(inputs) {
-    if (inputs.value = "") {
-        controlador++;
+function validarVacio() {
+    console.log("validando vacios");
+    if (!password1.value || !password2.value || !nombre.value || !apellido.value || !email.value) {
+        console.log("hay vacios");
+        return false;
+    } else {
+        return true;
     }
 }
 
 //Chechar que las contrseñas sean iguales
 function validarContrasenias() {
-    password1.value = password2.value;
+    console.log("Comparando contrasenias");
+    if (password1.value === password2.value){
+        console.log("contrasenias iguales");
+        return true;
+    } else {
+        console.log("contrasenias diferentes");
+        return false;
+    }
 }
 
 function cantidadDecaracteres() {
-    password1.length >= 6;
+    console.log("Viendo caracteres de contrasenias");
+    if (password1.value.length >= 6){
+        console.log("la contrasenia tiene 6 caracteres")
+        return true;
+    } else {
+        console.log("la contrasenia es muy corta")
+        return false;
+    }
+}
+
+function terminosChecked() {
+    if(terminos.checked){
+        return true;
+    } else {
+        return false;
+    }
+    
 }
 
 //Accion del boton
